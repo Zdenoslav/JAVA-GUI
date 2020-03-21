dd351edb1fae5aa22390c43ac7aa32f27f62981f
ZDENKO MUCINA <MucinaZ@cardiff.ac.uk>
2020-03-21 11:10:30 +0000

    app

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import javafx.application.Application;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application{
 
@@ -17,10 +19,21 @@ public class Main extends Application{
 
         Stage stage2 = new Stage();
         stage2.setTitle("Second window");
-        stage2.setWidth(1920);
-        stage2.setHeight(1080);
+        stage2.setWidth(400);
+        stage2.setHeight(300);
+
+        stage2.setX(50);
+        stage2.setY(100);
         stage2.show();
 
+        Stage newWindow = new Stage();
+        newWindow.initStyle(StageStyle.UNDECORATED);
+
+        //Modality
+        newWindow.initModality(Modality.WINDOW_MODAL);
+        newWindow.initOwner(stage2);
+
+        newWindow.show();
 
     }
 
