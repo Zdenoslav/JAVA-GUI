6122bec8bace961610b65f906c8fd2c531c1d0b8
ZDENKO MUCINA <MucinaZ@cardiff.ac.uk>
2020-03-26 17:58:12 +0000

    new exercise on Menu items

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 
-import java.awt.*;
+
 
 
 public class Main extends Application{
@@ -29,17 +32,47 @@ public class Main extends Application{
 
         VBox root = new VBox();
 
-        Hyperlink link = new Hyperlink("click me boy");
+        MenuItem item1 = new MenuItem("Taco");
+        MenuItem item2 = new MenuItem("Burrito");
+        MenuItem item3 = new MenuItem("Cheese insalda");
+        MenuItem item4 = new MenuItem( " Coca coal");
 
-        link.setOnAction(e -> {
-            System.out.println("thie link works");
+        MenuButton menuButton = new MenuButton("Favourtite Mexican food", null, item1, item2, item3, item4);
+        Label food = new Label("No food selected");
+
+        item1.setOnAction(e -> {
+            food.setText("Food: Taco");
         });
 
-        root.getChildren().addAll(link);
+        item2.setOnAction(e -> {
+            food.setText("Food: burrito");
+        });
+
+        item3.setOnAction(e -> {
+            food.setText("Food: Cheese");
+        });
+
+        item4.setOnAction(e -> {
+            food.setText("Food: Salad");
+        });
+
+        ToggleButton button1 = new ToggleButton("Blue");
+        ToggleButton button2 = new ToggleButton("Pink");
+        ToggleButton button3 = new ToggleButton(" Purple");
+        ToggleGroup  buttonGroup = new ToggleGroup();
+        button1.setToggleGroup(buttonGroup);
+        button2.setToggleGroup(buttonGroup);
+        button3.setToggleGroup(buttonGroup);
+
+        RadioButton r1 = new RadioButton("Brown");
+        RadioButton r2 = new RadioButton("Green");
+        r1.setToggleGroup(buttonGroup);
+        r2.setToggleGroup(buttonGroup);
+
+        root.getChildren().addAll(menuButton, food, button1, button2, button3, r1, r2);
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
-
 
         stage.show();
     }
